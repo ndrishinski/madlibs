b6eea8440e6a074200c0a32a9a93a0d8886a08f5
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-28 10:06:19 -0600

    added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Nick's MadLibs</h1>
+        <div className="backgroundSkew">
+          <div className="madlib-heading">
+            <h1>Nick's MadLibs</h1>
+             <div className="madlib-subheading">
+             Fil out the fields below and click the generate button<br/>to generate the button to see the MadLib story.
+             </div>
+          </div>
+        </div>
       </div>
     );
   }
