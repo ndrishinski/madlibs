1e28a6b8a6980c7f194c657fed7b19c6309ee297
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-28 10:22:20 -0600

    created madlib form component

---
+++ src/components/madlib_form.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class MadlibForm extends Component {
+    render() {
+        return <h1>Nick's MadLibs Form Component</h1>;
+    }
+}
+
+export default MadlibForm;
