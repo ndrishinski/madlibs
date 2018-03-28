a4b8a6ed7c9623cafd87b9a07f92c4e99849c664
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-28 11:38:55 -0600

    Modified handle change function to acccept and render text in the console

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,8 +1,98 @@
-import React, {Component} from 'react';
+
+import React, { Component } from 'react';
+import {
+    Col,
+    Row
+} from 'reactstrap';
 
 class MadlibForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: ''
+        }
+
+    }
+
+    handleChange = function(props) {
+        return function(event) {
+            // console.log(`value for input ${props.inputTitle} is: ${event.target.value}`)
+            this.setState({[props.inputTitle]: event.target.value});
+            console.log(`value for state ${props.inputTitle} is: ${this.state[props.inputTitle]}`)
+        }.bind(this);
+    }
+
+
     render() {
-        return <h1>Nick's MadLibs Form Component</h1>;
+      return (
+        <Row style={{textAlign: 'center', color: 'white'}}>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Color' type='text' onChange={this.handleChange({inputTitle: 'color'})} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Color</div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Noun (Plural)' type='text' onChange={this.handleChange({inputTitle: 'pluralNoun'})} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Noun (Plural)</div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Adjective' type='text' onChange={this.handleChange({inputTitle: 'adjectiveOne'})} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Adjective</div>
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Celebrity' type='text' onChange={this.handleChange({inputTitle: 'celebrityOne'})} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="12">
+                        <div className="inputDescription">Celebrity</div>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+      );
     }
 }
 
